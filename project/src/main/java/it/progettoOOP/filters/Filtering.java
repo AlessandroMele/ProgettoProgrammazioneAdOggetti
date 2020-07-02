68f09ab976ce83279c9fbb80f1cfe795cb1c5f9d
Matteo Verdecchia <s1090181@studenti.univpm.it>
2020-07-02 12:22:22 +0200

    piccole modifiche

--- project/src/main/java/it/progettoOOP/filters/Filtering.java
+++ project/src/main/java/it/progettoOOP/filters/Filtering.java
@@ -7,11 +7,11 @@ public class Filtering {
 	public ArrayListFacebookPost FilteredPosts(ArrayListFacebookPost array, FiltersModel options) {
 		ArrayListFacebookPost arrayfil = new ArrayListFacebookPost();
 		for (int i=0;i< array.getSize(); i++) {
-			if (options.getDescrizione()== true) {
+			//if (options.getDescrizione()== true) {
 				if (array.getPost(i).getLengthMessage() < options.getMax() && array.getPost(i).getLengthMessage() > options.getMin()) 
 				arrayfil.addPost(array.getPost(i));	
 				}
-			}
+			//}
 		
 		return arrayfil;
 	}
