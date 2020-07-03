e9883d16ded112f9cfb3536dce778f438c58a17e
Matteo Verdecchia <s1090181@studenti.univpm.it>
2020-07-03 15:27:37 +0200

    parte 2

--- project/src/main/java/it/progettoOOP/filters/Filters.java
+++ project/src/main/java/it/progettoOOP/filters/Filters.java
@@ -6,14 +6,17 @@ public class Filters {
 
 	public ArrayListFacebookPost FilteredPosts(ArrayListFacebookPost array, FiltersModel options) {
 		ArrayListFacebookPost arrayfil = new ArrayListFacebookPost();
-		
-		for (int i=0;i< array.getSize(); i++) {
-			if (array.getPost(i).getLengthMessage() <= options.getMaxLengthMess() && array.getPost(i).getLengthMessage() >= options.getMinLengthMess()
-				&& array.getPost(i).getNumShares() <= options.getMaxShares() && array.getPost(i).getNumShares() >= options.getMinShares()
-				&& array.getPost(i).getNumReactions() <= options.getMaxReactions() && array.getPost(i).getNumReactions() >= options.getMinReactions()) 
-				arrayfil.addPost(array.getPost(i));	
-				}
-	return arrayfil;
+
+		for (int i = 0; i < array.getSize(); i++) {
+			if (array.getPost(i).getLengthMessage() <= options.getMaxLengthMess()
+					&& array.getPost(i).getLengthMessage() >= options.getMinLengthMess()
+					&& array.getPost(i).getNumShares() <= options.getMaxShares()
+					&& array.getPost(i).getNumShares() >= options.getMinShares()
+					&& array.getPost(i).getNumReactions() <= options.getMaxReactions()
+					&& array.getPost(i).getNumReactions() >= options.getMinReactions())
+				arrayfil.addPost(array.getPost(i));
+		}
+		return arrayfil;
 	}
-	
+
 }
--- project/src/main/java/it/progettoOOP/filters/FiltersModel.java
+++ project/src/main/java/it/progettoOOP/filters/FiltersModel.java
@@ -1,6 +1,5 @@
 package it.progettoOOP.filters;
 
-
 import org.json.JSONException;
 import org.json.simple.JSONObject;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -10,121 +9,98 @@ public class FiltersModel {
 	private JSONObject shares;
 	private JSONObject reactions;
 	private boolean viewMessage;
-	//private boolean emoji;
-	
-	
+
 	public FiltersModel() {
 		try {
-		this.length = length;
-		}
-		catch(HttpMessageNotReadableException e) {
+			this.length = length;
+		} catch (HttpMessageNotReadableException e) {
 			this.length = null;
 		}
 		try {
-		this.shares = shares;
-		}
-		catch(HttpMessageNotReadableException e) {
+			this.shares = shares;
+		} catch (HttpMessageNotReadableException e) {
 			this.shares = null;
 		}
 		try {
 			this.reactions = reactions;
-		}
-		catch(HttpMessageNotReadableException e) {
+		} catch (HttpMessageNotReadableException e) {
 			this.reactions = null;
 		}
 		try {
-		this.viewMessage = false;
-		}
-		catch(HttpMessageNotReadableException e) {
+			this.viewMessage = false;
+		} catch (HttpMessageNotReadableException e) {
 			this.viewMessage = false;
 		}
-		/*try {
-		this.emoji = false;
-		}
-		catch(HttpMessageNotReadableException e) {
-			this.emoji = false;
-		}*/
-		
 	}
-	
-	public Boolean getDescrizione() {
+
+	public Boolean getViewMessage() {
 		return viewMessage;
 	}
-	
-	/*public boolean getEmoji() {
-		return emoji;
-	}*/
-	
+
 	public JSONObject getLength() {
 		return length;
 	}
-	
+
 	public JSONObject getShares() {
 		return shares;
 	}
-	
+
 	public JSONObject getReactions() {
 		return reactions;
 	}
-	
+
 	public int getMinLengthMess() {
 		try {
-		int minLengthMess =  (int) length.get("min");
-		return minLengthMess ;
-		}
-		catch(JSONException e) {
+			int minLengthMess = (int) length.get("min");
+			return minLengthMess;
+		} catch (JSONException e) {
 			return 0;
 		}
 	}
-	
+
 	public int getMaxLengthMess() {
 		try {
-		int maxLengthMess =  (int) length.get("max");
-		return maxLengthMess ;
-		}
-		catch(JSONException e) {
+			int maxLengthMess = (int) length.get("max");
+			return maxLengthMess;
+		} catch (JSONException e) {
 			return 100000;
 		}
 	}
-	
+
 	public int getMinShares() {
 		try {
-		int minShares =  (int) shares.get("min");
-		return minShares ;
-		}
-		catch(NullPointerException e) {
+			int minShares = (int) shares.get("min");
+			return minShares;
+		} catch (NullPointerException e) {
 			return 0;
 		}
 	}
-	
+
 	public int getMaxShares() {
 		try {
-		int maxShares =  (int) shares.get("max");
-		return maxShares ;
-		}
-		catch(NullPointerException e) {
+			int maxShares = (int) shares.get("max");
+			return maxShares;
+		} catch (NullPointerException e) {
 			return 100000;
 		}
 	}
-	
+
 	public int getMinReactions() {
 		try {
-		int minReactions =  (int) reactions.get("min");
-		return minReactions ;
-		}
-		catch(NullPointerException e) {
+			int minReactions = (int) reactions.get("min");
+			return minReactions;
+		} catch (NullPointerException e) {
 			return 0;
 		}
 	}
-	
+
 	public int getMaxReactions() {
 		try {
-		int maxReactions =  (int) reactions.get("max");
-		return maxReactions ;
-		}
-		catch(NullPointerException e) {
+			int maxReactions = (int) reactions.get("max");
+			return maxReactions;
+		} catch (NullPointerException e) {
 			return 100000;
 		}
 	}
-	
+
 }
